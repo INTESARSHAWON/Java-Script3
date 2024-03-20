4cfd50ef1405b5d6ef95cf21c3d60e5426cf7866
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-20 21:16:01 +0600

    Conditional Statements

---
+++ script.js
@@ -0,0 +1,18 @@
+//Logical Operatiors
+
+//  && || !- eigula same to same as C programming language
+
+//Conditional Statements
+
+let number= prompt ("Enter a Number");
+console.log (number);
+
+if (number%5===0){
+    console.log(number, "is multiplied by 5");
+}
+
+else {
+    console.log(number, "isn't multiplied by 5");
+}
+
+
