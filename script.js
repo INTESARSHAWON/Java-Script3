adbf5a21fb9b511773200a814a29816e10cff3e9
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-20 21:18:09 +0600

    disable previous code

--- script.js
+++ script.js
@@ -4,15 +4,15 @@
 
 //Conditional Statements
 
-let number= prompt ("Enter a Number");
-console.log (number);
+// let number= prompt ("Enter a Number");
+// console.log (number);
 
-if (number%5===0){
-    console.log(number, "is multiplied by 5");
-}
+// if (number%5===0){
+//     console.log(number, "is multiplied by 5");
+// }
 
-else {
-    console.log(number, "isn't multiplied by 5");
-}
+// else {
+//     console.log(number, "isn't multiplied by 5");
+// }
 
 
