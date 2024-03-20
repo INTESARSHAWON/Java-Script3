a77c56138059278bc8171c3e176062c3a61dbc41
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-20 21:19:09 +0600

    solving another problem

--- script.js
+++ script.js
@@ -15,4 +15,25 @@
 //     console.log(number, "isn't multiplied by 5");
 // }
 
+let marks= prompt ("Enter the marks");
+console.log ("marks");
+
+if (marks>=90 && marks<=100 ){
+    console.log (marks,"Grade is A");
+}
+else if (marks>=70 && marks<=89 ){
+    console.log (marks,"Grade is B");
+}
+else if (marks>=60 && marks<=69 ){
+    console.log (marks,"Grade is C");
+}
+else if (marks>=50 && marks<=59 ){
+    console.log (marks,"Grade is D");
+}
+else if (marks>=0 && marks<=49 ){
+    console.log (marks,"Grade is F");
+}
+else {
+    console.log ("marks isn' valid");
+}
 
